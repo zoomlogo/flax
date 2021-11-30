dfc79def263eaa61e2083807a5a3e970a0e9340c
PyGamer0 <64531844+PyGamer0@users.noreply.github.com>
2021-11-30 14:14:06 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='flax',
+    version='0.0.0',
+    description='Flax tacit language',
+    url='https://github.com/PyGamer0/flax',
+    author='PyGamer0',
+    license='MIT',
+    packages=['flax'],
+    install_requires=['numpy'],
+    scripts=['scripts/flax']
+)
