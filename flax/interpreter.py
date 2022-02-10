0b58c7ffa5f8db5594106f2327b158633c8154da
PyGamer0 <maanasyt@gmail.com>
2022-02-10 12:14:24 +0530

    outer product

--- flax/interpreter.py
+++ flax/interpreter.py
@@ -127,7 +127,7 @@ def from_bin(x):
     num = 0
     i = 0
     for b in x[::-1]:
-        num += abs(b) * 2**i
+        num += abs(b) * 2 ** i
         i += 1
     return num * sign
 
@@ -138,7 +138,7 @@ def from_digits(x):
     num = 0
     i = 0
     for b in x[::-1]:
-        num += abs(b) * 10**i
+        num += abs(b) * 10 ** i
         i += 1
     return num * sign
 
@@ -360,7 +360,7 @@ atoms = {
     "Ă": attrdict(arity=1, call=contains_false),
     "B": attrdict(arity=1, call=vectorised(to_bin)),
     "Ḃ": attrdict(arity=1, call=from_bin),
-    "Ḅ": attrdict(arity=1, call=vectorised(lambda a: 2**a)),
+    "Ḅ": attrdict(arity=1, call=vectorised(lambda a: 2 ** a)),
     "Ƀ": attrdict(arity=1, call=vectorised(lambda a: a % 2)),
     "C": attrdict(arity=1, call=vectorised(lambda a: 1 - a)),
     "Ċ": attrdict(arity=1, call=vectorised(lambda a: a * 3)),
@@ -380,7 +380,7 @@ atoms = {
     "Ĵ": attrdict(arity=1, call=join_newlines),
     "K": attrdict(arity=1, call=lambda x: scanl1(op.add, iterable(x))),
     "L": attrdict(arity=1, call=len),
-    "M": attrdict(arity=1, call=vectorised(lambda a: a**2)),
+    "M": attrdict(arity=1, call=vectorised(lambda a: a ** 2)),
     "N": attrdict(arity=1, call=vectorised(lambda a: -a)),
     "O": attrdict(arity=1, call=lambda x: x),
     "P": attrdict(arity=1, call=lambda x: flax_print(x)),
@@ -830,6 +830,19 @@ quicks = {
     ),
     "´": attrdict(condition=lambda links: links and links[0].arity, qlink=qfold),
     "`": attrdict(condition=lambda links: links and links[0].arity, qlink=qscan),
+    "⌜": attrdict(
+        condition=lambda links: links,
+        qlink=lambda links, outer_links, i: [
+            attrdict(
+                arity=2,
+                call=lambda x, y: outer_product(
+                    links[0].call,
+                    iterable(x, make_range=True),
+                    iterable(y, make_range=True),
+                ),
+            )
+        ],
+    ),
     "ⁿ": attrdict(
         condition=lambda links: len(links) == 2,
         qlink=lambda links, outer_links, i: (
