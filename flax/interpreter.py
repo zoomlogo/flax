a6af9baa0f3fb970af31fd3b313a46fac215b82d
PyGamer0 <maanasyt@gmail.com>
2022-02-20 11:07:07 +0530

    alternative glyphs for (pre/suf)fix

--- flax/interpreter.py
+++ flax/interpreter.py
@@ -134,7 +134,7 @@ def from_bin(x):
     num = 0
     i = 0
     for b in x[::-1]:
-        num += abs(b) * 2**i
+        num += abs(b) * 2 ** i
         i += 1
     return num * sign
 
@@ -145,7 +145,7 @@ def from_digits(x):
     num = 0
     i = 0
     for b in x[::-1]:
-        num += abs(b) * 10**i
+        num += abs(b) * 10 ** i
         i += 1
     return num * sign
 
@@ -407,7 +407,7 @@ atoms = {
     "Æ": attrdict(arity=1, call=vectorised(compose(int, sympy.isprime))),
     "B": attrdict(arity=1, call=vectorised(to_bin)),
     "Ḃ": attrdict(arity=1, call=from_bin),
-    "Ḅ": attrdict(arity=1, call=vectorised(lambda a: 2**a)),
+    "Ḅ": attrdict(arity=1, call=vectorised(lambda a: 2 ** a)),
     "Ƀ": attrdict(arity=1, call=vectorised(lambda a: a % 2)),
     "C": attrdict(arity=1, call=vectorised(lambda a: 1 - a)),
     "Ċ": attrdict(arity=1, call=vectorised(lambda a: a * 3)),
@@ -427,7 +427,7 @@ atoms = {
     "Ĵ": attrdict(arity=1, call=join_newlines),
     "K": attrdict(arity=1, call=lambda x: scanl1(op.add, iterable(x))),
     "L": attrdict(arity=1, call=len),
-    "M": attrdict(arity=1, call=vectorised(lambda a: a**2)),
+    "M": attrdict(arity=1, call=vectorised(lambda a: a ** 2)),
     "N": attrdict(arity=1, call=vectorised(lambda a: -a)),
     "O": attrdict(arity=1, call=lambda x: x),
     "P": attrdict(arity=1, call=lambda x: flax_print(x)),
@@ -481,8 +481,8 @@ atoms = {
     "⅟": attrdict(arity=1, call=vectorised(lambda a: 1 / a if a else sympy.oo)),
     "⌈": attrdict(arity=1, call=vectorised(lambda a: ceil(a))),
     "⌊": attrdict(arity=1, call=vectorised(lambda a: floor(a))),
-    "⊢": attrdict(arity=1, call=prefixes),
-    "⊣": attrdict(arity=1, call=suffixes),
+    "(": attrdict(arity=1, call=prefixes),
+    ")": attrdict(arity=1, call=suffixes),
     "∀": attrdict(arity=1, call=lambda x: [*map(sum, x)]),
     # Single byte dyads
     "c": attrdict(arity=2, call=lambda x, y: iterable(x, make_digits=True).count(y)),
