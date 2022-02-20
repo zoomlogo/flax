4465d7c6f9e4b374a4359a777e3724cf1e3c69f6
PyGamer0 <maanasyt@gmail.com>
2022-02-20 11:10:21 +0530

    argv fix

--- flax/__main__.py
+++ flax/__main__.py
@@ -1,34 +1,33 @@
 # Main entry point
-from sys import argv
 from prompt_toolkit import print_formatted_text, HTML
 import sys
 
 from flax.main import flax_eval, cli_repl
 import flax.interpreter
 
-argv = argv[1:]
+sys.argv = argv[1:]
 read_from_file = False
 
-if argv:
-    if "d" in argv[0]:
+if sys.argv:
+    if "d" in sys.argv[0]:
         flax.interpreter.DEBUG = True
-    if "f" in argv[0]:
+    if "f" in sys.argv[0]:
         read_from_file = True
-    if "C" in argv[0]:
+    if "C" in sys.argv[0]:
         flax.interpreter.PRINT_CHARS = True
-    argv = argv[1:]
+    sys.argv = argv[1:]
 
 if read_from_file:
     try:
-        code = open(argv[0], encoding="utf-8").read()
+        code = open(sys.argv[0], encoding="utf-8").read()
     except FileNotFoundError:
         print_formatted_text(
-            HTML(f"<ansired>ERROR: File {argv[0]} not found.</ansired>"),
+            HTML(f"<ansired>ERROR: File {sys.argv[0]} not found.</ansired>"),
             file=sys.stderr,
         )
         exit(66)
 
-    argv = argv[1:]
-    flax_eval(code, *map(eval, argv))
+    sys.argv = argv[1:]
+    flax_eval(code, *map(eval, sys.argv))
 else:
     cli_repl()
