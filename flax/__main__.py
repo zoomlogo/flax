26ed0a75e2b6416657b0ff0653239a6c5a445021
PyGamer0 <maanasyt@gmail.com>
2022-03-05 12:23:56 +0530

    flag to increase precision

--- flax/__main__.py
+++ flax/__main__.py
@@ -15,6 +15,8 @@ if sys.argv:
         read_from_file = True
     if "C" in sys.argv[0]:
         flax.interpreter.PRINT_CHARS = True
+    if "p" in sys.argv[0]:
+        flax.interpreter.dps = 100
     sys.argv = sys.argv[1:]
 
 if read_from_file:
