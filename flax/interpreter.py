70eb39e0fa4d3b508f02dc5ce6a05731a2036a9e
PyGamer0 <maanasyt@gmail.com>
2022-03-12 15:30:46 +0530

    300 commits

--- flax/interpreter.py
+++ flax/interpreter.py
@@ -140,7 +140,7 @@ def from_bin(x):
     num = 0
     i = 0
     for b in x[::-1]:
-        num += abs(b) * 2**i
+        num += abs(b) * 2 ** i
         i += 1
     return num * sign
 
@@ -151,7 +151,7 @@ def from_digits(x):
     num = 0
     i = 0
     for b in x[::-1]:
-        num += abs(b) * 10**i
+        num += abs(b) * 10 ** i
         i += 1
     return num * sign
 
@@ -416,10 +416,10 @@ atoms = {
     "Æ": attrdict(arity=1, call=vecd(lambda a: int(mp.isprime(a)))),
     "B": attrdict(arity=1, call=vecd(to_bin)),
     "Ḃ": attrdict(arity=1, call=from_bin),
-    "Ḅ": attrdict(arity=1, call=vecd(lambda a: 2**a)),
+    "Ḅ": attrdict(arity=1, call=vecd(lambda a: 2 ** a)),
     "Ƀ": attrdict(arity=1, call=vecd(lambda a: a % 2)),
     "C": attrdict(arity=1, call=vecd(lambda a: 1 - a)),
-    "Ċ": attrdict(arity=1, call=vecd(lambda a: a**3)),
+    "Ċ": attrdict(arity=1, call=vecd(lambda a: a ** 3)),
     "Ç": attrdict(arity=1, call=lambda x: split(x, 2)),
     "D": attrdict(arity=1, call=vecd(to_digits)),
     "Ḋ": attrdict(arity=1, call=from_digits),
@@ -445,7 +445,7 @@ atoms = {
     "Ĵ": attrdict(arity=1, call=join_newlines),
     "K": attrdict(arity=1, call=lambda x: list(it.accumulate(iterable(x)))),
     "L": attrdict(arity=1, call=len),
-    "M": attrdict(arity=1, call=vecd(lambda a: a**2)),
+    "M": attrdict(arity=1, call=vecd(lambda a: a ** 2)),
     "N": attrdict(arity=1, call=vecd(lambda a: -a)),
     "O": attrdict(arity=1, call=lambda x: x),
     "P": attrdict(arity=1, call=lambda x: list(it.permutations(x))),
@@ -645,9 +645,9 @@ atoms = {
     "_v": attrdict(arity=0, call=lambda: to_chars("aeiou")),
     "_y": attrdict(arity=0, call=lambda: to_chars("aeiouy")),
     "_∞": attrdict(arity=0, call=lambda: mp.inf),
-    "_⁰": attrdict(arity=0, call=lambda: 2**20),
-    "_¹": attrdict(arity=0, call=lambda: 2**30),
-    "_²": attrdict(arity=0, call=lambda: 2**100),
+    "_⁰": attrdict(arity=0, call=lambda: 2 ** 20),
+    "_¹": attrdict(arity=0, call=lambda: 2 ** 30),
+    "_²": attrdict(arity=0, call=lambda: 2 ** 100),
     "_(": attrdict(arity=0, call=lambda: to_chars("()")),
     "_{": attrdict(arity=0, call=lambda: to_chars("{}")),
     "_[": attrdict(arity=0, call=lambda: to_chars("[]")),
@@ -676,9 +676,13 @@ atoms = {
     ";R": attrdict(arity=1, call=vecd(lambda a: list(range(int(a) + 1)))),
     ";√": attrdict(arity=1, call=vecd(lambda a: int(mp.sqrt(a)))),
     ";j": attrdict(arity=1, call=vecd(lambda a: [mp.mpc(a).real, mp.mpc(a).imag])),
-    ";J": attrdict(arity=1, call=vecd(lambda a: mp.mpc(a[0], a[1]))),
+    ";J": attrdict(
+        arity=1, call=vecd(lambda a: [mp.mpc(z[0], z[1]) for z in split(flatten(a), 2)])
+    ),
     ";L": attrdict(arity=1, call=vecd(mp.log)),
     ";E": attrdict(arity=1, call=vecd(mp.exp)),
+    ";°": attrdict(arity=1, call=vecd(mp.radians)),
+    ";P": attrdict(arity=1, call=lambda x: list(mp.polyroots(flatten(x)))),
     # Dyadic diagraphs
     ":T": attrdict(arity=2, call=vecd(mp.atan2)),
     ":<": attrdict(arity=2, call=vecd(lambda a, b: a << b)),
