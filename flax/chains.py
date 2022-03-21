1779e215b33607ee0cf04905a0b7379da16cbc38
PyGamer0 <maanasyt@gmail.com>
2022-03-21 16:03:20 +0530

    dyadic chain

--- flax/chains.py
+++ flax/chains.py
@@ -1,5 +1,5 @@
 # chains: holds the functions used by quicks and the chains
-from flax.common import attrdict
+from flax.common import attrdict, flax_print
 
 
 def arities(links):
@@ -22,6 +22,47 @@ def create_chain(chain, arity=-1, isForward=True):
             chain, *(isForward and (w, x) or (x, w))
         ),
     )
+
+
+def dyadic_chain(chain, w, x):
+    # dyadic_chain: evaluate a dyadic chain
+    for link in chain:
+        if link.arity < 0:
+            link.arity = 2
+
+    if chain and arities(chain[-3:]) == [2, 2, 2]:
+        λ = chain[-1].call(w, x)
+        chain = chain[:-1]
+    elif trailing_nilad(chain):
+        λ = chain[-1].call()
+        chain = chain[:-1]
+    else:
+        λ = x
+
+    while chain:
+        if arities(chain[-3:]) == [0, 2, 2] and trailing_nilad(chain[:-2]):
+            λ = chain[-2].call(chain[-3].call(), chain[-1].call(w + λ))
+            chain = chain[:-3]
+        elif arities(chain[-2:]) == [2, 2]:
+            λ = chain[-1].call(chain[-2].call(x, w), λ)
+            chain = chain[:-2]
+        elif arities(chain[-2:]) == [0, 2]:
+            λ = chain[-1].call(chain[-2].call(), λ)
+            chain = chain[:-2]
+        elif arities(chain[-2:]) == [0, 2]:
+            λ = chain[-2].call(λ, chain[-1].call())
+            chain = chain[:-2]
+        elif chain[-1].arity == 2:
+            λ = chain[-1].call(w, λ)
+            chain = chain[:-1]
+        elif chain[-1].arity == 1:
+            λ = chain[-1].call(λ)
+            chain = chain[:-1]
+        else:
+            flax_print(λ)
+            λ = chain[-1].call()
+            chain = chain[:-1]
+    return λ
 
 
 def trailing_nilad(chain):
