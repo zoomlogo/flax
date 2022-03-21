2b40a2ec35780a7fba10bad0119fa4636bc4fbbb
PyGamer0 <maanasyt@gmail.com>
2022-03-21 18:59:15 +0530

    add builtins.py and make lexer use them

--- flax/lexer.py
+++ flax/lexer.py
@@ -3,8 +3,7 @@ import collections
 import enum
 import string
 
-from flax.interpreter import atoms
-from flax.interpreter import quicks
+from flax.builtins import *
 
 __all__ = ["TOKEN_TYPE", "tokenise"]
 
@@ -26,54 +25,54 @@ def tokenise(program):
 
     while program:
         head = program.popleft()
-        if head == "'":
+        if head == STRING_DELIMETER:
             string_contents = ""
             while program:
                 top = program.popleft()
                 if top == "\\" and program:
                     string_contents += "\\" + program.popleft()
-                elif top == "'":
+                elif top == STRING_DELIMETER:
                     break
                 else:
                     string_contents += top
             tokens.append([TOKEN_TYPE.STRING, string_contents])
-        elif head == "₊":
+        elif head == STRING_NEXT_1:
             string_contents = program.popleft()
             tokens.append([TOKEN_TYPE.STRING, string_contents])
-        elif head == "₋":
+        elif head == STRING_NEXT_2:
             string_contents = program.popleft()
             string_contents += program.popleft()
             tokens.append([TOKEN_TYPE.STRING, string_contents])
-        elif head in string.digits + "¯.j":
+        elif head in DIGITS:
             contextual_token_value = head
-            if head == "0" and not (program and program[0] in "¯.j"):
+            if head == ZERO and not (program and program[0] in DECIMAL_POINT + COMPLEX_DELIMETER + NEGATIVE_SIGN):
                 # handle the special case of 0
                 tokens.append([TOKEN_TYPE.NUMBER, contextual_token_value])
             else:
                 while (
                     program
-                    and program[0] in string.digits + "¯.j"
-                    and (contextual_token_value + program[0]).count("j") < 2
+                    and program[0] in DIGITS
+                    and (contextual_token_value + program[0]).count(COMPLEX_DELIMETER) < 2
                     and all(
-                        (x.count(".") < 2 and x.count("¯") < 2)
-                        for x in (contextual_token_value + program[0]).split("j")
+                        (x.count(DECIMAL_POINT) < 2 and x.count(NEGATIVE_SIGN) < 2)
+                        for x in (contextual_token_value + program[0]).split(COMPLEX_DELIMETER)
                     )
                 ):
                     contextual_token_value += program.popleft()
                 tokens.append([TOKEN_TYPE.NUMBER, contextual_token_value])
-        elif head == "⍝":  # ideally these string constants will be in builtins
+        elif head == COMMENT:
             # just ignore comments
-            while program and program.popleft() != "\n":
+            while program and program.popleft() != NEWLINE:
                 pass
-        elif head == "\n":
-            tokens.append([TOKEN_TYPE.NEWLINE, "\n"])
-        elif head in "øµðɓг":
+        elif head == NEWLINE:
+            tokens.append([TOKEN_TYPE.NEWLINE, NEWLINE])
+        elif head in train_separators:
             tokens.append([TOKEN_TYPE.TRAIN_SEPARATOR, head])
         elif head in atoms:
             tokens.append([TOKEN_TYPE.ATOM, head])
         elif head in quicks:
             tokens.append([TOKEN_TYPE.QUICK, head])
-        elif head in "_;:ᵟ" and program:
+        elif head in DIAGRPAHS and program:
             digraph = head + program.popleft()
             if digraph in atoms:
                 tokens.append([TOKEN_TYPE.ATOM, digraph])
@@ -81,12 +80,12 @@ def tokenise(program):
                 tokens.append([TOKEN_TYPE.QUICK, digraph])
             else:
                 raise NameError("Digraph not defined.")
-        elif head == "[":
+        elif head == LIST_DELIMETER_L:
             contents = ""
             k = 1
             while True:
                 head = program.popleft()
-                if head == "]":
+                if head == LIST_DELIMETER_R:
                     k -= 1
                     if k == 0:
                         tokens.append([TOKEN_TYPE.LIST, tokenise(contents)])
@@ -94,7 +93,7 @@ def tokenise(program):
                     else:
                         contents += head
                 else:
-                    if head == "[":
+                    if head == LIST_DELIMETER_L:
                         k += 1
                     contents += head
     return tokens
