7f71f9b5950329a49e969448408d5177d7cc6b2c
Caleb Miller <caleb@mathmammoth.com>
2022-05-17 11:36:12 -0600

    csv

--- docs/json-to-csv.js
+++ docs/json-to-csv.js
@@ -1,7 +1,7 @@
-const tsv = require('tsv');
+const csv = require('json2csv');
 const fs = require('fs');
 const path = require('path');
 
 const json = JSON.parse(fs.readFileSync(path.join(__dirname, "elements.json")).toString());
 
-fs.writeFileSync(path.join(__dirname, "elements.tsv"), tsv.stringify(json));
+fs.writeFileSync(path.join(__dirname, "elements.csv"), csv.parse(json));
