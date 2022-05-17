30746ab38d3c2751762f3d0ac38968695819e37f
Caleb Miller <caleb@mathmammoth.com>
2022-05-17 11:42:56 -0600

    autogenerate txt

--- docs/json-to-yaml.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const yaml = require('yaml');
-
-const json = JSON.parse(fs.readFileSync(path.join(__dirname, 'elements.json')).toString());
-
-fs.writeFileSync(path.join(__dirname, 'elements.yaml'), yaml.stringify(json).replace(/- /g, "\n- ").trim());
