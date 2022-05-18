83b827790517ffea64f9d883457c0bfcc7279513
PyGamer0 <64531844+PyGamer0@users.noreply.github.com>
2022-05-18 14:29:38 +0530

    add search
    lets hope it works

--- script.js
+++ script.js
@@ -8,8 +8,27 @@ const getJSON = async url => {
 /* load json */
 console.log("Fetching Data...");
 getJSON("https://raw.githubusercontent.com/PyGamer0/flax/main/docs/elements.json").then(elements => {
-  var el = document.getElementById("el");
+  let el = document.getElementById("el");
   elements.forEach(element => {
     el.insertAdjacentHTML('beforeend', "<tr><td>" + element["element"] + "</td><td>" + element["description"] + "</td><td>" + element["arity-type"] + "</td></tr>");
   });
 });
+
+/* search function */
+const filter_search = () => {
+  let bar = document.getElementById("search-bar");
+  let filter = bar.value.toLowerCase();
+  let table = document.getElementById("el");
+  let rows = table.getElementsByTagName("tr");
+
+  rows.forEach(row => {
+    let d = row.getElementsByTagName("td")[1];
+    if (d) {
+      let value = (d.textContent || d.innerText).toLowerCase();
+      if (value.indexOf(filter) > -1)
+        row.style.display = "";
+      else
+        row.style.display = "none";
+    }
+  })
+}
