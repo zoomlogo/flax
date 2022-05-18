7945c54887b93c4e9949bb42b75cebb527aa0bfb
PyGamer0 <64531844+PyGamer0@users.noreply.github.com>
2022-05-18 11:53:55 +0530

    reversed smh

--- script.js
+++ script.js
@@ -10,6 +10,6 @@ console.log("Fetching Data...");
 getJSON("https://raw.githubusercontent.com/PyGamer0/flax/main/docs/elements.json").then(elements => {
   var el = document.getElementById("el");
   elements.forEach(element => {
-    el.insertAdjacentHTML('afterend', "<div id=\"el-item\">" + element["element"] + "</div>");
+    el.insertAdjacentHTML('beforeend', "<div id=\"el-item\">" + element["element"] + "</div>");
   });
 });
