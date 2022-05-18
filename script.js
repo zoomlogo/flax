a750bfbc51b2afcbcc70e6293f16974a9150d5db
PyGamer0 <64531844+PyGamer0@users.noreply.github.com>
2022-05-18 11:37:54 +0530

    test

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 console.log("Loaded!");
 
+const getJSON = async url => {
+    const response = await fetch(url);
+    const data = response.json();
+    return data;
+}
+
 /* load json */
-fetch("https://raw.githubusercontent.com/PyGamer0/flax/main/docs/elements.json");
+console.log("Fetching Data...");
+getJSON("https://raw.githubusercontent.com/PyGamer0/flax/main/docs/elements.json").then(
+    data => { console.log(data); }
+)
