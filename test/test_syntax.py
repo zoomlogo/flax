fc43e7fc1fa673d870d5384b3bbf67fbfb0f1cff
PyGamer0 <PyGamer0@users.noreply.github.com>
2022-10-07 05:47:01 +0000

    Format files with black

--- test/test_syntax.py
+++ test/test_syntax.py
@@ -2,16 +2,19 @@
 # and niladic atoms
 import os, sys
 from mpmath import *
+
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__)) + "/.."
 sys.path.insert(1, THIS_FOLDER)
 
 from flax.main import flax_run as run
+
 
 def test_numbers_egde_cases():
     assert run("¯") == -1
     assert run(".") == 0.5
     assert run("i") == mpc("0", "1")
     assert run(".i¯.") == mpc("0.5", "-0.5")
+
 
 def test_numbers():
     assert run("0") == 0
@@ -20,8 +23,24 @@ def test_numbers():
     assert run("69i420") == mpc("69", "420")
     assert run("69.420") == mpf("69.420")
 
+
 def test_string():
-    assert run('"Hello, World!"') == [72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33]
+    assert run('"Hello, World!"') == [
+        72,
+        101,
+        108,
+        108,
+        111,
+        44,
+        32,
+        87,
+        111,
+        114,
+        108,
+        100,
+        33,
+    ]
+
 
 def test_next_n():
     assert run("_E") == [69]
