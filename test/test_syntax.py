817f61ffbb58995c2fd2c7ea4ecefd1b028564cb
PyGamer0 <PyGamer0@users.noreply.github.com>
2022-10-08 11:43:33 +0000

    Format files with black

--- test/test_syntax.py
+++ test/test_syntax.py
@@ -51,5 +51,5 @@ def test_lists():
     assert run("[1]") == [1]
     assert run("[1, 2]") == [1, 2]
     assert run("[1+2]") == [1, 2]  # any delimeter
-    assert run("[[1 2],[3 4]]") == [[1,2],[3,4]]
-    assert run('["Hi" 1]') == [[72,105],1]
+    assert run("[[1 2],[3 4]]") == [[1, 2], [3, 4]]
+    assert run('["Hi" 1]') == [[72, 105], 1]
