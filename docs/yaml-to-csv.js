1e785e8045953f54d5363bf9a7cddd962a7ab54b
PyGamer0 <64531844+PyGamer0@users.noreply.github.com>
2022-11-05 11:38:21 +0530

    Delete yaml-to-csv.js

--- docs/yaml-to-csv.js
+++
@@ -1,8 +0,0 @@
-const csv = require('json2csv');
-const yaml = require('yaml');
-const fs = require('fs');
-const path = require('path');
-
-const json = yaml.parse(fs.readFileSync(path.join(__dirname, "elements.yaml")).toString());
-
-fs.writeFileSync(path.join(__dirname, "elements.csv"), csv.parse(json));
