5f40e4f7fa0c29cdb12b3bf52558fcd6da44a17e
PyGamer0 <64531844+PyGamer0@users.noreply.github.com>
2022-11-05 11:38:28 +0530

    Delete yaml-to-txt.js

--- docs/yaml-to-txt.js
+++
@@ -1,8 +0,0 @@
-const yaml = require('yaml');
-const fs = require('fs');
-const path = require('path');
-const columnify = require('columnify');
-
-const json = yaml.parse(fs.readFileSync(path.join(__dirname, "elements.yaml")).toString());
-
-fs.writeFileSync(path.join(__dirname, "elements.txt"), columnify(json).split('\n').map(x => x.trimEnd()).join('\n'));
