95c73461aa1b3bdde1f92b53dd3283bf06ac16e9
PyGamer0 <64531844+PyGamer0@users.noreply.github.com>
2022-11-05 11:42:48 +0530

    Update yaml-to.js

--- docs/yaml-to.js
+++ docs/yaml-to.js
@@ -7,5 +7,5 @@ const columnify = require('columnify');
 const json = yaml.parse(fs.readFileSync(path.join(__dirname, "elements.yaml")).toString());
 
 fs.writeFileSync(path.join(__dirname, "elements.csv"), csv.parse(json));  // csv
-fs.writeFileSync(path.join(__dirname, 'elements.json'), JSON.stringify(y, null, "  "));  // json
+fs.writeFileSync(path.join(__dirname, 'elements.json'), JSON.stringify(json, null, "  "));  // json
 fs.writeFileSync(path.join(__dirname, "elements.txt"), columnify(json).split('\n').map(x => x.trimEnd()).join('\n'));  // txt
