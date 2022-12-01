acbd300a73c4a1c50c54109f95d2213db08dbe5f
PyGamer0 <PyGamer0@users.noreply.github.com>
2022-12-01 11:34:29 +0000

    Format files with black

--- flax/chains.py
+++ flax/chains.py
@@ -259,7 +259,7 @@ def group(links, *args):
     """group: sort group according to links"""
     args = [i for i in args if i is not None]
     x = iterable(args[-1], digits_=True)
-    gx = list(map(lambda i: variadic_chain(links, (i, )), x))
+    gx = list(map(lambda i: variadic_chain(links, (i,)), x))
     # TODO: lens(links) == 2 case
     res = group_indicies(gx)
     return res
