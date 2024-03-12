64fcafb4a7704df58b7a188b0f5c9319fdc0f74b
zoomlogo <zoomlogo@users.noreply.github.com>
2024-03-12 16:41:17 +0000

    Format files with black

--- test/test_funcs.py
+++ test/test_funcs.py
@@ -185,29 +185,34 @@ def test_fibonacci():
 
 
 def test_find():
-    assert find(3, [1,2])==[]
-    assert find(3, [1,2,3]) == 2
-    assert find(3, [1,2,3,4,5,3]) == 2
+    assert find(3, [1, 2]) == []
+    assert find(3, [1, 2, 3]) == 2
+    assert find(3, [1, 2, 3, 4, 5, 3]) == 2
+
 
 def test_find_md():
-    assert find_md(4, [[1,2,3,4],[3,4,5],4,6]) == [0, 3]
-    assert find_md(3, [1,2]) == []
-    assert find_md(3, [1,3]) == [1]
+    assert find_md(4, [[1, 2, 3, 4], [3, 4, 5], 4, 6]) == [0, 3]
+    assert find_md(3, [1, 2]) == []
+    assert find_md(3, [1, 3]) == [1]
+
 
 def test_find_all():
-    assert find_all(3, [1,2])==[]
-    assert find_all(3, [1,2,3]) == [2]
-    assert find_all(3, [1,2,3,4,5,3]) == [2,5]
+    assert find_all(3, [1, 2]) == []
+    assert find_all(3, [1, 2, 3]) == [2]
+    assert find_all(3, [1, 2, 3, 4, 5, 3]) == [2, 5]
+
 
 def test_find_sublist():
-    assert find_sublist([1,2,3, 4,5],[1,2,2,1,2,3,4,2,1]) == []
-    assert find_sublist([1,2,3],[1,2,2,1,2,3,4,2,1]) == 3
-    assert find_sublist([1,2,3,4],[1,2,2,1,2,3,4,2,1]) == 3
+    assert find_sublist([1, 2, 3, 4, 5], [1, 2, 2, 1, 2, 3, 4, 2, 1]) == []
+    assert find_sublist([1, 2, 3], [1, 2, 2, 1, 2, 3, 4, 2, 1]) == 3
+    assert find_sublist([1, 2, 3, 4], [1, 2, 2, 1, 2, 3, 4, 2, 1]) == 3
+
 
 def test_flatten():
-    assert flatten([1,2,[3,4,[5,3],5],[],3]) == [1,2,3,4,5,3,5,3]
+    assert flatten([1, 2, [3, 4, [5, 3], 5], [], 3]) == [1, 2, 3, 4, 5, 3, 5, 3]
     assert flatten([]) == []
     assert flatten(1) == [1]
+
 
 "test_get_req"
 "test_grade_down"
